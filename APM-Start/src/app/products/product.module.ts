89773b4975d1f751ad6e06336b18700a8c8d7d0f
Mike Parker <mikeparker@crescerance.com>
2019-04-10 20:15:54 -0400

    productsroute

--- APM-Start/src/app/products/product.module.ts
+++ APM-Start/src/app/products/product.module.ts
@@ -5,10 +5,15 @@ import { ProductDetailComponent } from './product-detail.component';
 import { ProductEditComponent } from './product-edit/product-edit.component';
 
 import { SharedModule } from '../shared/shared.module';
+import { RouterModule, Route } from '@angular/router';
 
+const Routes: Route[] = [
+  { path: 'products', component: ProductListComponent }
+];
 @NgModule({
   imports: [
-    SharedModule
+    SharedModule,
+    RouterModule.forChild(Routes)
   ],
   declarations: [
     ProductListComponent,
