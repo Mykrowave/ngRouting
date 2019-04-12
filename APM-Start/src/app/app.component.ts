8a188b9c3a182db67da217e92ce10aa40ee02123
Mike Parker <mikeparker@crescerance.com>
2019-04-11 22:57:01 -0400

    animation

--- APM-Start/src/app/app.component.ts
+++ APM-Start/src/app/app.component.ts
@@ -2,12 +2,13 @@ import { Component } from '@angular/core';
 
 import { AuthService } from './user/auth.service';
 import { Router } from '@angular/router';
-import { routerNgProbeToken } from '@angular/router/src/router_module';
+import { slideInAnimation } from './app.animation';
 
 @Component({
   selector: 'pm-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  animations: [slideInAnimation]
 })
 export class AppComponent {
   pageTitle = 'Acme Product Management';
